77be7cbc5bb1d03da42368b73ef28a52755f8661
강성훈 <yolly050101@naver.com>
2021-10-17 23:30:30 +0900

    feat: Overlapping

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Temp from "./Temp";
+
+const LoginPage = () => {
+  return <Temp></Temp>;
+};
+
+export default LoginPage;
---
+++ src/pages/Temp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/temp.css";
+
+const Temp = ({ children }) => {
+  return <div className="temp">{children}</div>;
+};
+
+export default Temp;
