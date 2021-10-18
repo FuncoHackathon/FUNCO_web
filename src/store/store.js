8017e9e2da7a381843435a088bf7e0abe0f65801
강성훈 <yolly050101@naver.com>
2021-10-18 15:09:29 +0900

    feat: fundingReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from "redux";
 import rootreducer from "./reducer";
 import userReducer from "./userReducer";
+import fundingReducer from "./funding";
 
 const reducer = combineReducers({
   rootreducer,
   userReducer,
+  fundingReducer,
 });
 
 const store = createStore(reducer);
