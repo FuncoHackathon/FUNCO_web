6a9413c3a73313b8bf43c0d42ff8791c0fcb7213
강성훈 <yolly050101@naver.com>
2021-10-18 14:02:22 +0900

    feat: signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
   const isLog = useSelector((state) => state.rootreducer.isLog);
@@ -9,8 +10,8 @@ const App = () => {
     <>
       {!isLog ? (
         <BrowserRouter>
-          <Route path="/" component={LoginPage} />
-          <Route path="/signup" component={LoginPage} />
+          <Route path="/" exact component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
         </BrowserRouter>
       ) : (
         <BrowserRouter>
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignForm from "../components/SignForm";
+import Temp from "../pages/Temp";
+
+const SignUp = () => {
+  return (
+    <Temp>
+      <SignForm />
+    </Temp>
+  );
+};
+
+export default SignUp;
