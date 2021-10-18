7e3c0007909908fb0cdae32c4e80c8388568cda7
강성훈 <yolly050101@naver.com>
2021-10-18 12:04:33 +0900

    feat: login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,41 @@
-import React, { useState } from "react";
+import React, { memo, useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../img/logo.png";
 import "../styles/join.scss";
 import "../styles/common.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { LOGIN } from "../store/reducer";
 
-const LoginForm = () => {
+const LoginForm = memo(() => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const member = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const onChangeEmail = useCallback(
+    (e) => {
+      setEmail(e.target.value);
+    },
+    [email]
+  );
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+    },
+    [password]
+  );
+  const onClickLogin = useCallback(
+    (e) => {
+      e.preventDefault();
+      const find = member.find((v) => email === v.email);
+      if (find) {
+        dispatch({ type: LOGIN, name: find.userName });
+      }
+      setPassword("");
+      setEmail("");
+    },
+    [email, password]
+  );
 
   return (
     <>
@@ -15,13 +44,25 @@ const LoginForm = () => {
         <form className="userIDPW">
           <label className="label">
             <div className="inputFrame">이메일</div>
-            <input type="email" className="userInfo" />
+            <input
+              type="email"
+              className="userInfo"
+              value={email}
+              onChange={onChangeEmail}
+            />
           </label>
           <label className="label">
             <div className="inputFrame">비밀번호</div>
-            <input type="password" className="userInfo" />
+            <input
+              type="password"
+              className="userInfo"
+              value={password}
+              onChange={onChangePassword}
+            />
           </label>
-          <button className="loginBt">로그인</button>
+          <button className="loginBt" onClick={onClickLogin}>
+            로그인
+          </button>
         </form>
       </div>
       <p className="noaccount">
@@ -32,6 +73,6 @@ const LoginForm = () => {
       </p>
     </>
   );
-};
+});
 
 export default LoginForm;
