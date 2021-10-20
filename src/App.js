6568b1c3b32e13ff7415ade36ded029e892db662
강성훈 <yolly050101@naver.com>
2021-10-20 19:05:23 +0900

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import axios from "axios";
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import MyFunding from "./components/MyFunding";
 import Ranking from "./components/Ranking";
 import SomeFunding from "./components/SomeFunding";
+import apiconfig from "./config/apiconfig";
 import AddPage from "./pages/AddPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import SignUpPage from "./pages/SignUpPage";
+import { LOGIN } from "./store/reducer";
 
 const App = withRouter(({ history }) => {
   const isLog = useSelector((state) => state.rootreducer.isLog);
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    if (localStorage.getItem("jwtToken")) {
+      const res = await axios.get(`${apiconfig.API_ENDPOINT}/users/name`, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+        },
+      });
+      dispatch({ type: LOGIN, name: res.data.name });
+    } else {
+      return;
+    }
+  }, []);
 
   useEffect(() => {
     if (isLog === false) {
